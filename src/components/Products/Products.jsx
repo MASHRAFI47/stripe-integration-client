e06aa2dc3856bed1445a35448bb52f529b587860
MASHRAFI47 <mbngms@gmail.com>
2024-08-19 05:17:11 +0600

    product brand added

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -6,22 +6,25 @@ const Products = () => {
     const [currentPage, setCurrentPage] = useState(1);
     const [count, setCount] = useState(0);
 
+    const [search, setSearch] = useState("");
+    const [brand, setBrand] = useState("");
+
     const [products, setProducts] = useState([]);
     useEffect(() => {
         const getData = async () => {
-            const { data } = await axios(`${import.meta.env.VITE_api_url}/products?itemsPerPage=${itemsPerPage}&currentPage=${currentPage}`)
+            const { data } = await axios(`${import.meta.env.VITE_api_url}/products?itemsPerPage=${itemsPerPage}&currentPage=${currentPage}&brand=${brand}&search=${search}`)
             setProducts(data);
         }
         getData();
-    }, [itemsPerPage, currentPage]);
+    }, [itemsPerPage, currentPage, brand, search]);
 
     useEffect(() => {
         const getData = async () => {
-            const { data } = await axios(`${import.meta.env.VITE_api_url}/products-count`)
+            const { data } = await axios(`${import.meta.env.VITE_api_url}/products-count?brand=${brand}&search=${search}`)
             setCount(data.count);
         }
         getData();
-    }, []);
+    }, [brand, search]);
 
 
 
@@ -29,16 +32,29 @@ const Products = () => {
         setCurrentPage(val)
     }
 
+
     const numberOfPages = Math.ceil(count / itemsPerPage);
     const pages = [...Array(numberOfPages).keys()].map(element => element + 1);
 
+
     return (
         <div className="container mx-auto">
+            <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" onChange={e => setSearch(e.target.value)} />
+            <br />
+            <br />
+
+            <select className="select select-bordered w-full max-w-xs" onChange={e => setBrand(e.target.value)} value={brand}>
+                <option disabled selected>Who shot first?</option>
+                <option>MSI</option>
+                <option>Asus</option>
+                <option>Lenovo</option>
+            </select>
+
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
                 {
                     products?.map(product => <div key={product?._id}>
                         <div>
-                            <div className="card bg-base-100 w-96 shadow-xl">
+                            <div className="card bg-base-100 shadow-xl border mt-3">
                                 <figure>
                                     <img
                                         src={product?.productImage}
