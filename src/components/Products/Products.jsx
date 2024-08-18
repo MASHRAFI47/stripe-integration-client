8145bc9ecbc89b5e3dbf23a7436a79b8cb56dba1
MASHRAFI47 <mbngms@gmail.com>
2024-08-18 18:44:32 +0600

    products added

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,7 +1,43 @@
+import axios from "axios";
+import { useEffect, useState } from "react"
 
 const Products = () => {
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        const getData = async () => {
+            const { data } = await axios(`${import.meta.env.VITE_api_url}/products`)
+            setProducts(data);
+        }
+        getData();
+    }, []);
+
+    console.log(products)
+
     return (
-        <div>Products</div>
+        <div className="container mx-auto">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+                {
+                    products?.map(product => <div key={product?._id}>
+                        <div>
+                            <div className="card bg-base-100 w-96 shadow-xl">
+                                <figure>
+                                    <img
+                                        src={product?.productImage}
+                                        alt="Shoes" />
+                                </figure>
+                                <div className="card-body">
+                                    <h2 className="card-title">{product?.productName}</h2>
+                                    <p>Price: ${product?.price}</p>
+                                    <div className="card-actions justify-end">
+                                        <button className="btn btn-primary">Buy Now</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>)
+                }
+            </div>
+        </div>
     )
 }
 
