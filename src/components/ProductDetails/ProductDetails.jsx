773e7aab1896df6900bd73449e0b015f4bc213c3
MASHRAFI47 <mbngms@gmail.com>
2024-08-20 09:13:25 +0600

    stripe card form added

--- src/components/ProductDetails/ProductDetails.jsx
+++ src/components/ProductDetails/ProductDetails.jsx
@@ -2,6 +2,12 @@ import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom"
 import useAxiosCommon from "../../hooks/useAxiosCommon";
 
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckoutForm from "../Form/CheckoutForm";
+
+const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY);
+
 const ProductDetails = () => {
     const { id } = useParams();
     const axiosCommon = useAxiosCommon();
@@ -20,7 +26,21 @@ const ProductDetails = () => {
         <div className="container mx-auto">
             <img src={product?.productImage} className="w-96" alt="" />
             <p className="text-2xl"><span className="font-bold">Price:</span> ${product?.price}</p>
-            <button className="btn btn-primary">Buy Now</button>
+            {/* You can open the modal using document.getElementById('ID').showModal() method */}
+            <button className="btn" onClick={() => document.getElementById('my_modal_3').showModal()}>Buy Now</button>
+            <dialog id="my_modal_3" className="modal">
+                <div className="modal-box">
+                    <form method="dialog">
+                        {/* if there is a button in form, it will close the modal */}
+                        <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                    </form>
+                    <Elements stripe={stripePromise}>
+                        <CheckoutForm product={product} />
+                    </Elements>
+                    {/* <h3 className="font-bold text-lg">Hello!</h3>
+                    <p className="py-4">Press ESC key or click on ✕ button to close</p> */}
+                </div>
+            </dialog>
         </div>
     )
 }
